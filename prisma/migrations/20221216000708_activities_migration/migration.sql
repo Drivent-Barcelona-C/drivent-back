4b4191558f4f7deb0ad4d3956fbcf397489c776d
mmparadinha <matheusmparadinha@gmail.com>
2022-12-15 21:10:01 -0300

    Create activities prisma entities and dump folder

--- prisma/migrations/20221216000708_activities_migration/migration.sql
+++ prisma/migrations/20221216000708_activities_migration/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "ActivityLocation" AS ENUM ('AUDITORIO_PRINCIPAL', 'AUDITORIO_LATERAL', 'SALA_DE_WORKSHOP');
+
 -- CreateTable
 CREATE TABLE "Hotel" (
     "id" SERIAL NOT NULL,
@@ -32,6 +35,31 @@ CREATE TABLE "Booking" (
     CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "startHour" TIMESTAMP(3) NOT NULL,
+    "endHour" TIMESTAMP(3) NOT NULL,
+    "location" "ActivityLocation" NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ActivityBooking" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "activityId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ActivityBooking_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "Room" ADD CONSTRAINT "Room_hotelId_fkey" FOREIGN KEY ("hotelId") REFERENCES "Hotel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -40,3 +68,9 @@ ALTER TABLE "Booking" ADD CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Booking" ADD CONSTRAINT "Booking_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActivityBooking" ADD CONSTRAINT "ActivityBooking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActivityBooking" ADD CONSTRAINT "ActivityBooking_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
